179655cad6f0229b6948a838b78162a3627f5262
Ricardo Alexandro Mena <ricardoamena@gmail.com>
2024-04-08 16:47:13 -0300

    finalizo imagenes mini, boton borrar y demas, parte admin

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+import os
 from flask import Flask
 from flask import render_template, request, redirect
 from flaskext.mysql import MySQL
+from datetime import datetime
+from flask import send_from_directory
 
 app=Flask(__name__)
 mysql=MySQL()
+app.config['UPLOAD_FOLDER'] = 'templates/sitio/imagenes'
 
 app.config['MYSQL_DATABASE_HOST']='localhost'
 app.config['MYSQL_DATABASE_USER']='root'
@@ -14,6 +18,10 @@ mysql.init_app(app)
 @app.route('/')
 def inicio():
     return render_template('sitio/index.html')
+
+@app.route("/imagenes/<imagen>")
+def imagenes(imagen):
+    return send_from_directory(os.path.join('templates/sitio/imagenes'), imagen)
 
 @app.route('/libros')
 def libros():
@@ -49,17 +57,30 @@ def admin_libros_guardar():
     _archivo=request.files['txtImagen']
     _url=request.form['txtUrl']
     
-    sql="INSERT INTO libros (id, nombre, imagen, url) VALUES (NULL, %s, %s, %s);"
-    datos=(_nombre, _archivo.filename, _url)
+    tiempo= datetime.now()
+    horaActual=tiempo.strftime('%Y%H%M$S')
+
+    if _archivo.filename != "":
+        nuevoNombre = horaActual + "_" + _archivo.filename
+        _archivo.save(os.path.join(app.config['UPLOAD_FOLDER'], nuevoNombre))  # Usa os.path.join para manejar correctamente las rutas
+    
+    sql = "INSERT INTO libros (id, nombre, imagen, url) VALUES (NULL, %s, %s, %s);"
+    datos = (_nombre, nuevoNombre, _url)
     
     conexion=mysql.connect()
     cursor=conexion.cursor()
     cursor.execute(sql,datos)
     conexion.commit()
     
-    print(_nombre)
-    print(_url)
-    print(_archivo)
+    return redirect('/admin/libros')
+
+@app.route('/admin/libros/borrar', methods=['POST'])
+def admin_libros_borrar():
+    _id=request.form['txtID']
+    conexion=mysql.connect()
+    cursor= conexion.cursor()
+    cursor.execute("DELETE FROM libros WHERE `id` = %s", (_id))
+    conexion.commit()
     return redirect('/admin/libros')
 
 if __name__=='__main__':
